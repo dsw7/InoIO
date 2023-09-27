990a6a281134409d29ab7cfc0ddf8e675aa52dc2
David Weber <dsw7@sfu.ca>
2023-09-27 00:47:27 -0700

    Add errors

--- inoio/serial_connection.py
+++ inoio/serial_connection.py
@@ -1,28 +1,61 @@
-import sys
 from logging import getLogger
 from time import sleep
 from typing import Tuple
 import serial
+from inoio import errors
 
 
 class InoIO:
+
+    """Class for interfacing with an Arduino device.
+
+    :param int, optional baudrate: Specify the baud rate.
+    :param float, optional timeout: Specify the read timeout in sections.
+    :param str, optional port: Specify the device name (i.e. "COM3" on Windows) or path to device
+        (i.e. "/dev/ttyS2" on Linux).
+    """
+
     logger = getLogger("inoio")
 
     def __init__(
         self,
         baudrate: int = 9600,
-        encoding: str = "utf-8",
         port: str = "/dev/ttyS2",
         timeout: float = 5.00,
     ) -> None:
         self.baudrate = baudrate
-        self.encoding = encoding
+        self.encoding = "utf-8"
         self.port = port
         self.timeout = timeout
 
         self.serial_port_obj: serial.Serial
 
+    def init_app(
+        self,
+        baudrate: int,
+        port: str,
+        timeout: float,
+    ) -> None:
+        """Override parameters specified via class constructor. This method is useful
+        for setting connection parameters if the InoIO class is being used as a Flask extension,
+        for example. For more information, see https://flask.palletsprojects.com/en/2.3.x/extensions/
+
+        :param int, optional baudrate: Specify the baud rate.
+        :param float, optional timeout: Specify the read timeout in sections.
+        :param str, optional port: Specify the device name (i.e. "COM3" on Windows) or path to device
+            (i.e. "/dev/ttyS2" on Linux).
+        """
+
+        self.baudrate = baudrate
+        self.port = port
+        self.timeout = timeout
+
     def connect(self) -> None:
+        """Connect to device.
+
+        :raise InoIOConnectionError: If a connection could not be established.
+        """
+
         try:
             self.serial_port_obj = serial.Serial(
                 baudrate=self.baudrate,
@@ -34,11 +67,13 @@ class InoIO:
                 parity=serial.PARITY_NONE,
                 stopbits=serial.STOPBITS_ONE,
             )
-        except serial.serialutil.SerialException as exception:
-            sys.exit(f'An exception occurred when connecting: "{exception}"')
+        except serial.serialutil.SerialException as e:
+            raise errors.InoIOConnectionError(
+                f"Could not connect on {self.port}"
+            ) from e
 
         if not self.serial_port_obj.is_open:
-            sys.exit(f'Could not connect to "{self.serial_port_obj.name}"')
+            raise errors.InoIOConnectionError(f"No connection is open on {self.port}")
 
         # Opening a connection will send a DTR (Data Terminal Ready) signal to device, which will
         # force the device to reset. Give device 2 seconds to reset
@@ -48,9 +83,11 @@ class InoIO:
         )
         sleep(2)
 
-        self.logger.debug("Device ready to accept instructions!")
+        self.logger.debug("Device ready to accept input on %s", self.port)
 
     def disconnect(self) -> None:
+        """Disconnect from device."""
+
         if self.serial_port_obj is None:
             self.logger.debug("Not closing connection. Connection was never opened!")
             return
