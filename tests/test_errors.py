e623bde59d9c58e0582474b5491a4cf201421476
dsw7 <dds3443@hotmail.com>
2023-09-27 23:29:01 -0700

    Separate tests

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -2,33 +2,6 @@ from pytest import raises
 from inoio import InoIO, errors
 
 
-def test_invalid_port() -> None:
-    conn = InoIO(port=27117)  # type: ignore
-
-    with raises(errors.InoIOConnectionError) as excinfo:
-        conn.connect()
-
-    assert "One or more parameters is of invalid type" in str(excinfo)
-
-
-def test_invalid_baudrate() -> None:
-    conn = InoIO(baudrate="foobar")  # type: ignore
-
-    with raises(errors.InoIOConnectionError) as excinfo:
-        conn.connect()
-
-    assert "One or more parameters is of invalid type" in str(excinfo)
-
-
-def test_invalid_timeout() -> None:
-    conn = InoIO(timeout="foobar")  # type: ignore
-
-    with raises(errors.InoIOConnectionError) as excinfo:
-        conn.connect()
-
-    assert "One or more parameters is of invalid type" in str(excinfo)
-
-
 def test_connect_disconnect_write(pytestconfig) -> None:
     port = pytestconfig.getoption("port")
 
